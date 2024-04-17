ab147aefd48bfc6d4393c1731b1647845d76bf88
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2024-04-17 22:43:41 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,72 +1,31 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let myBalance = 25000;
-let mypin = 1122;
-console.log("Welcome to Taha's ATM Machine");
-let pinAnswer = await inquirer.prompt([
-    {
-        name: "pin",
-        type: "number",
-        message: "Enter your pin number"
-    }
-]);
-if (pinAnswer.pin === mypin) {
-    console.log("Login successful");
-    let operationAns = await inquirer.prompt([
+
+let condition = true;
+let todos = [];
+
+while (condition) {
+    const todoQuestion = await inquirer.prompt([
         {
-            name: "operation",
-            message: "Please select an option",
-            type: "list",
-            choices: ["withdraw", "check balance", "fastcash"]
-        }
+            name: "firstQuestion",
+            type: "input",
+            message: "What would you like to add in your todos?",
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Would you like to add more in your todos?",
+            default: true,
+        },
     ]);
-    if (operationAns.operation === "withdraw") {
-        let amountAns = await inquirer.prompt([
-            {
-                name: "amount",
-                type: "number",
-                message: "Enter your amount to withdraw"
-            }
-        ]);
-        if (amountAns.amount > myBalance) {
-            console.log("Insufficient balance");
-        }
-        else {
-            myBalance -= amountAns.amount;
-            console.log(`${amountAns.amount} withdraw successful`);
-            console.log(`Your remaining balance is: ${myBalance}`);
-        }
-    }
-    else if (operationAns.operation === "check balance") {
-        console.log(`Your current balance is: ${myBalance}`);
-    }
-    if (operationAns.operation === "fastcash") {
-        let Fastcash = await inquirer.prompt([
-            {
-                message: "Select one operation",
-                name: "cash",
-                type: 'list',
-                choices: [2000, 5000, 10000, 20000]
-            },
-        ]);
-        if (Fastcash.cash === 2000) {
-            console.log("Thank you for using our bank");
-            console.log("2,000 successfully transferred to your account");
-        }
-        else if (Fastcash.cash === 5000) {
-            console.log("Thank you for using our bank");
-            console.log("5,000 successfully transferred to your account");
-        }
-        else if (Fastcash.cash === 10000) {
-            console.log("Thank you for using our bank");
-            console.log("10,000 successfully transferred to your account");
-        }
-        else {
-            console.log("Thank you for using our bank");
-            console.log("20,000 successfully transferred to your account");
-        }
-    }
+
+    todos.push(todoQuestion.firstQuestion);
+    console.log(todos);
+    condition = todoQuestion.secondQuestion;
 }
-else {
-    console.log("Incorrect pin number");
+
+// you need all things in length so do like this   
+//(note) add things one by one for better work
+for (let i = 0; i < todos.length; i++) {
+    console.log(`${i+1}. ${todos[i]}`);
 }
